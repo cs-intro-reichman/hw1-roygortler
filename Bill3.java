547157ce0d9f5004bbd19a0ecbb043193d51a601
רועי גורטלר <roy.gortler@post.runi.ac.il>
2024-11-07 12:25:39 +0200

    testing

--- Bill3.java
+++ Bill3.java
@@ -6,7 +6,7 @@ public class Bill3 {
         String p3=args[2];
         int amount= Integer.parseInt(args[3]);
         double pay=Math.ceil((double)amount/3);
-        System.out.println("Dear "+p3+", "+p2+", "+p1+": pay "+pay+" shekels each");
+        System.out.println("Dear "+p3+", "+p2+", and "+p1+": pay "+pay+" shekels each");
         
         
     }
