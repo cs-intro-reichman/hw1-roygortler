4769ccd7e709ede98f1e9d8df48f9c0446bdcecf
רועי גורטלר <roy.gortler@post.runi.ac.il>
2024-11-07 12:43:31 +0200

    testing

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,13 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
-	}
+		int amount= Integer.parseInt(args[0]);
+		double percentage = Double.parseDouble(args[1]);
+		int years = Integer.parseInt(args[2]);
+		double yield= amount* (double)Math.pow(1+(percentage/100),years);
+		int yield1= (int)yield;
+		System.out.println( yield1);
+		System.out.println(amount+amount*(percentage/100));
+	
+}
 }
--- NumWords.java
+++ NumWords.java
@@ -2,7 +2,8 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    int x = Integer.parseInt(args[0]);
-        System.out.println(x/100+" hundreads "+x%100/10+" tens "+x%10+" ones ");
+        System.out.println(x/100+" hundreads, "+x%100/10+" tens, and "+x%10+" ones.");
+		0 hundreds, 0 tens, and 2 ones.
             
 	}
 }
