ddc3e6b68a75b930ecd4cb65c5bec2d2f6cc53b4
רועי גורטלר <roy.gortler@post.runi.ac.il>
2024-11-07 12:43:54 +0200

    testing

--- NumWords.java
+++ NumWords.java
@@ -3,7 +3,7 @@ public class NumWords {
 	public static void main(String args[]) {
 	    int x = Integer.parseInt(args[0]);
         System.out.println(x/100+" hundreads, "+x%100/10+" tens, and "+x%10+" ones.");
-		0 hundreds, 0 tens, and 2 ones.
+		
             
 	}
 }
