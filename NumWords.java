b033888c592274e9e0855ec9f88d704832e6f422
רועי גורטלר <roy.gortler@post.runi.ac.il>
2024-11-07 12:21:31 +0200

    testing

--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,8 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int x = Integer.parseInt(args[0]);
+        System.out.println(x/100+" hundreads "+x%100/10+" tens "+x%10+" ones ");
+            
 	}
 }
